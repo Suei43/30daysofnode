8c7beb36825d3d24764553ea23749de5893df605
Suei43 <chrisraphael576@gmail.com>
2023-07-14 14:45:37 +0100

    day 1

---
+++ tsfiles/day1.ts
@@ -0,0 +1,28 @@
+const server = require("http")
+const port = 3000
+const fs = require('fs')
+
+console.log("Hello World");
+
+server.createServer((req: object,res: any)=>{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    console.log("Server is running");
+    if(req){
+        console.log(typeof req)
+    }
+    else{
+        console.log("Empty request")
+    }
+    res.write("First res sent\n")
+    fs.readFile('index.html',(err: object | string | null, data: object|string|null)=>{
+        if(err)
+            throw err;
+        console.log("Html file successfully sent!")
+        res.end(data);
+    })
+}).listen(3000,(err: object)=>{
+    if(err){
+        return console.log(err)
+    }
+    console.log(`Server is running on port: ${port}`)
+})
