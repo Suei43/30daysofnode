27a710e8ca05dc40d6e0f9f5068ff2d1508e7cbf
Suei43 <chrisraphael576@gmail.com>
2023-07-21 16:11:32 +0100

    Working man: day 20

---
+++ src/day20.ts
@@ -0,0 +1,10 @@
+function log(){
+    console.log("Its quite a bright morning!")
+}
+console.log(1)
+setImmediate(log)
+log()
+setInterval(log,3000)
+setTimeout(log,3000)
+console.log(1)
+console.log(1)
