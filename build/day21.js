3dd368140a63d7559af9c1d03c55012dc42400cb
Suei43 <chrisraphael576@gmail.com>
2023-07-21 23:40:18 +0100

    Working man: day 21

---
+++ build/day21.js
@@ -0,0 +1,15 @@
+"use strict";
+let buffer = Buffer.alloc(10); // To create a buffer
+console.log(buffer);
+let unsafebuffer = Buffer.allocUnsafe(10); // To create a unsafe buffer
+console.log(unsafebuffer);
+console.log(typeof buffer);
+let bufferstr1 = Buffer.from('suei'); // To create a buffer from a string
+let bufferstr2 = Buffer.from('suei');
+console.log(bufferstr1.compare(bufferstr2)); // To compare two buffers - returns 0 or 1
+console.log(bufferstr1.equals(bufferstr2)); // To compare 2 buffers - returns true or false
+let newbuffer = Buffer.alloc(20);
+newbuffer.copy(buffer); // To copy one buffer to another
+let newbuffer2 = Buffer.alloc(20);
+newbuffer2 = buffer;
+console.log(newbuffer2 == buffer);
